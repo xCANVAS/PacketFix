6e3c3c0b95883ffd2dbea2a9a810ce965a58d80e
CANVAS <76691501+xCANVAS@users.noreply.github.com>
2023-12-10 16:04:19 +0800

    Update fixMotionJump.java

--- fixMotionJump.java
+++ fixMotionJump.java
@@ -1,3 +1,4 @@
+//In PacketFix.java
 //fixMotionJump(EntityLivingBase)
     public static double fixMotionJump() {
         if (ViaMCP.getInstance().getVersion() == ViaMCP.PROTOCOL_VERSION) {
@@ -6,3 +7,29 @@
             return 0.003D;
         }
     }
+
+//In EntityLivingBase.java
+//change
+        if (Math.abs(this.motionX) < 0.005) {
+            this.motionX = 0.0;
+        }
+
+        if (Math.abs(this.motionY) < 0.005) {
+            this.motionY = 0.0;
+        }
+
+        if (Math.abs(this.motionZ) < 0.005) {
+            this.motionZ = 0.0;
+        }
+//to
+        if (Math.abs(this.motionX) < Packetfix.fixMotionJump()) {
+            this.motionX = 0.0D;
+        }
+
+        if (Math.abs(this.motionY) < Packetfix.fixMotionJump()) {
+            this.motionY = 0.0D;
+        }
+
+        if (Math.abs(this.motionZ) < Packetfix.fixMotionJump()) {
+            this.motionZ = 0.0D;
+        }
